4f4290944a431e0a4e9812057a9ef25ed8c2ec51
unknown <lochuynh90@gmail.com>
2021-01-24 16:02:33 +0700

    added config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$cfg = new StdClass;
+$cfg->dbtype   = 'postgres8';
+$cfg->dbhost   = 'ec2-34-206-252-187.compute-1.amazonaws.com';
+$cfg->dbport   = 5432;
+$cfg->dbname   = 'dfdf4lg9t1ul79';
+$cfg->dbuser   = 'bptdfzottzhjal';
+$cfg->dbpass   = 'eeafb1a48a35595ab7d12b3dcf1c10db8a05cc4dd0ecab2e557a1a9d1b52dcfb';
+$cfg->dbprefix = '';
+$cfg->dataroot = '/app';
+$cfg->emailcontact = '';
